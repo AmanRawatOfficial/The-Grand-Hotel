1960f4601c34e0d3af67f0ffa3243ceaa528be29
AmanRawat <amanrawat4060@gmail.com>
2023-06-27 16:57:19 +0530

    Added functionality to delete and update the cabins data

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
-import { formatCurrency } from "../../utils/helpers.js";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCabin } from "../../services/apiCabins.js";
 import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+
+import { formatCurrency } from "../../utils/helpers.js";
+import { deleteCabin } from "../../services/apiCabins.js";
+import CreateCabinForm from "./CreateCabinForm.jsx";
 
 const TableRow = styled.div`
     display: grid;
@@ -44,6 +47,8 @@ const Discount = styled.div`
 `;
 
 function CabinRow({ cabin }) {
+    const [showForm, setShowForm] = useState(false);
+
     const {
         id: cabinId,
         name,
@@ -67,16 +72,27 @@ function CabinRow({ cabin }) {
     });
 
     return (
-        <TableRow role="row">
-            <Img src={image} />
-            <Cabin>{name}</Cabin>
-            <div>Fits up to {maxCapacity} quests</div>
-            <Price>{formatCurrency(regularPrice)}</Price>
-            <Discount>₹{discount}</Discount>
-            <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
-                Delete
-            </button>
-        </TableRow>
+        <>
+            <TableRow role="row">
+                <Img src={image} />
+                <Cabin>{name}</Cabin>
+                <div>Fits up to {maxCapacity} quests</div>
+                <Price>{formatCurrency(regularPrice)}</Price>
+                <Discount>₹{discount}</Discount>
+                <div>
+                    <button onClick={() => setShowForm((show) => !show)}>
+                        Edit
+                    </button>
+                    <button
+                        onClick={() => mutate(cabinId)}
+                        disabled={isDeleting}
+                    >
+                        Delete
+                    </button>
+                </div>
+            </TableRow>
+            {showForm && <CreateCabinForm cabinToEdit={cabin} />}
+        </>
     );
 }
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://lbuvslbljbyjhxwhgskf.supabase.co";
+export const supabaseUrl = "https://lbuvslbljbyjhxwhgskf.supabase.co";
 const supabaseKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxidXZzbGJsamJ5amh4d2hnc2tmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODc1MTk4MjMsImV4cCI6MjAwMzA5NTgyM30.bsvdBPqKF7UqwdqsFuOUKJNTd5pcscc_Ww22BhoIw_o";
 const supabase = createClient(supabaseUrl, supabaseKey);
